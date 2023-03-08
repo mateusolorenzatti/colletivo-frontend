f59f7df50adc2a58a52b35c83b3574563f27a73c
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2023-03-07 22:24:21 -0300

    feat(general): added routing, style configs, navbar and sign-in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: SignInComponent
+  }
+];
+
+/*
+  {
+    path: 'routes',
+    loadChildren: () => import('src/app/routes/routes.module').then(m => m.RoutesModule)
+  },
+  {
+    path: 'stops',
+    loadChildren: () => import('src/app/stops/stops.module').then(m => m.StopsModule)
+  },
+  {
+    path: 'trips',
+    loadChildren: () => import('src/app/trips/trips.module').then(m => m.TripsModule)
+  }
+*/
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    AuthModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
